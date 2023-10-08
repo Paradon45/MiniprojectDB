02de5853d70edd6608bfbbdf950e652f10a7f434
Paradon45 <143957067+Paradon45@users.noreply.github.com>
2023-10-09 01:34:20 +0700

    delete.php

--- Register2/delete.php
+++ Register2/delete.php
@@ -6,12 +6,17 @@
     $sql = "DELETE FROM post WHERE post_id = $id";
 
     $result = mysqli_query($conn, $sql) or die("Error in sql : $sql".mysqli_error($conn));
+
+    $sql_a = "DELETE FROM comment WHERE id = $id";
+
+    $result_a = mysqli_query($conn, $sql_a) or die("Error in sql : $sql".mysqli_error($conn));
+
     mysqli_close($conn);
 
     if($result){
         header('Location: profile.php');
     } else {
-        echo 'Erro !!';
+        echo 'Error !!';
     }
 
 ?>
